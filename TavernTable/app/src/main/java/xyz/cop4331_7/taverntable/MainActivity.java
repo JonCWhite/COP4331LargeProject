18c7b6ee92151accd3803821d829954aba900aca
Jon <jon@chipetto>
2018-07-09 14:17:01 -0400

    added back buttons and made existing buttons switch between layouts

--- TavernTable/app/src/main/java/xyz/cop4331_7/taverntable/MainActivity.java
+++ TavernTable/app/src/main/java/xyz/cop4331_7/taverntable/MainActivity.java
@@ -1,7 +1,10 @@
 package xyz.cop4331_7.taverntable;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Configure buttons to be pressed
+        configureReturnUserButton();
+        configureNewUserButton();
+    }
+
+    private void configureReturnUserButton() {
+        Button returnUserButton = (Button) findViewById(R.id.returnUserButton);
+        returnUserButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignInActivity.class));
+            }
+        }));
+    }
+
+    private void configureNewUserButton() {
+        Button newUserButton = (Button) findViewById(R.id.newUserButton);
+        newUserButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                }
+            }
+        );
     }
 }
