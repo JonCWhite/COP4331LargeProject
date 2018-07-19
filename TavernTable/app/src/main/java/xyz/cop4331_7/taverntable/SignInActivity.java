6b2e0d08535688acbbfaa9ddca78dcfc2343f144
Fernando <bilbaof107@gmail.com>
2018-07-19 14:58:35 -0400

    SignIn/SignUp updated

--- TavernTable/app/src/main/java/xyz/cop4331_7/taverntable/SignInActivity.java
+++ TavernTable/app/src/main/java/xyz/cop4331_7/taverntable/SignInActivity.java
@@ -1,102 +1,139 @@
 package xyz.cop4331_7.taverntable;
 
-import android.app.AlertDialog;
 import android.content.Intent;
-//import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class SignInActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
 
+public class SignInActivity extends AppCompatActivity
+{
+    Button signInButton;
+    EditText etUsername, etPassword;
+    FloatingActionButton backButton;
+    String username, password;
+    static final String signInURL = "http://cop4331-7.xyz/login.php";
+
+
+    // Begin sign in activity and configure UI elements.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        // Set variable values to the inputs in the Sign Up page.
-        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
-        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
-        final Button bSignIn = (Button) findViewById(R.id.bSignIn);
-        final Button bSignup = (Button) findViewById(R.id.bSignUp);
 
-        //configureBackButton();
+        signInButton = (Button) findViewById(R.id.signInButton);
+        etUsername = (EditText) findViewById(R.id.usernameField);
+        etPassword = (EditText) findViewById(R.id.passwordField);
+        backButton = (FloatingActionButton) findViewById(R.id.signInBackButton);
 
-        // When Sign Up button is clicked, go to the sign up page.
-        /*bSignup.setOnClickListener(new View.OnClickListener()
+
+
+        // Configure sign in and back buttons
+        configureBackButton();
+        configureSignInButton();
+    }
+
+    // Configure back button to end this activity and return to the main activity.
+    public void configureBackButton()
+    {
+        FloatingActionButton backButton = (FloatingActionButton) findViewById(R.id.signInBackButton);
+        backButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                Intent signUpIntent = new Intent(SignInActivity.this, RegisterActivity.class);
-                SignInActivity.this.startActivity(signUpIntent);
-            }
-        });*/
-
-        // When Sign In button is clicked, go to the Sign In page.
-        bSignIn.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                final String username = etUsername.getText().toString();
-                final String password = etPassword.getText().toString();
-
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response)
-                    {
-                        try
-                        {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-
-                            if(success)
-                            {
-
-                                Intent intent = new Intent(SignInActivity.this, UserAreaActivity.class);
-                                intent.putExtra("username", username);
-
-                                startActivity(intent);
-                            }
-                            else
-                            {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(SignInActivity.this);
-                                builder.setMessage("Login failed.")
-                                        .setNegativeButton("Try Again", null)
-                                        .create().show();
-                            }
-                        }
-                        catch (JSONException e)
-                        {
-                            e.printStackTrace();
-                        }
-                    }
-                };
-
-                SignInRequest loginRequest = new SignInRequest(username, password, responseListener);
-                RequestQueue queue = Volley.newRequestQueue(SignInActivity.this);
-                queue.add(loginRequest);
+                finish();
             }
         });
     }
 
-    /*public void configureBackButton() {
-        FloatingActionButton backButton = (FloatingActionButton) findViewById(R.id.signInBackButton);
-        backButton.setOnClickListener(new View.OnClickListener(){
+    public void configureSignInButton()
+    {
+        signInButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                finish();
+            public void onClick(View view)
+            {
+                username = etUsername.getText().toString();
+                password = etPassword.getText().toString();
+
+                StringRequest postRequest = new StringRequest(Request.Method.POST, signInURL,
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response)
+                            {
+                                try
+                                {
+                                    JSONObject jsonResponse = new JSONObject(response);
+                                    String userID = jsonResponse.getString("userID"),
+                                            error = jsonResponse.getString("error");
+
+                                    if(error.equals("This user does not exist"))
+                                    {
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(SignInActivity.this);
+                                        builder.setMessage("Login failed.")
+                                                .setNegativeButton("Try Again", null)
+                                                .create().show();
+                                    }
+
+                                    else
+                                    {
+
+                                        System.out.println("SUCCESS");
+                                        Intent intent = new Intent(SignInActivity.this, UserAreaActivity.class);
+                                        startActivity(intent);
+
+                                    }
+
+                                }
+                                catch (JSONException e)
+                                {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error)
+                            {
+                                error.printStackTrace();
+                            }
+                        })
+                {
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String> params = new HashMap<>();
+
+                        // POST params
+                        params.put("username", username);
+                        params.put("password", password);
+
+                        return params;
+                    }
+                };
+
+                Volley.newRequestQueue(getApplicationContext()).add(postRequest);
             }
         });
-    }*/
+    }
 }
---
+++ TavernTable/app/src/main/java/xyz/cop4331_7/taverntable/createCharacter.java
@@ -0,0 +1,38 @@
+package xyz.cop4331_7.taverntable;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class createCharacter extends AppCompatActivity
+{
+    /*private Spinner spinnerRace;
+    String[] race;
+    private ArrayList<String> racesContent = new ArrayList<String>();
+    FloatingActionButton backButton;
+    static final String createCharURL = "http://cop4331-7.xyz/CharacterSheet.php";
+    */
+
+}
