b53f37de19e0d131ddcc6c718765f9d01cd73807
johnb <johnabalbuza@gmail.com>
2018-07-22 13:11:00 -0400

    Latest build
    Mapping out logic to send notes

--- TavernTable/app/src/main/java/xyz/cop4331_7/taverntable/CharacterNotes.java
+++ TavernTable/app/src/main/java/xyz/cop4331_7/taverntable/CharacterNotes.java
@@ -6,8 +6,25 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CharacterNotes extends AppCompatActivity {
+
+    static final String url = "http://cop4331-7.xyz/system/notes.php";
+    EditText content;
+    String notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +34,57 @@ public class CharacterNotes extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //load in character notes
+
+
     }
 
+
+    @Override
+    public void onBackPressed(){
+
+        //save text currently in the text field
+        content = (EditText) findViewById(R.id.notesContent);
+        notes = content.getText().toString();
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            String userID = jsonResponse.getString("userID"),
+                                    error = jsonResponse.getString("error");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+
+                // POST parameters
+                params.put("notes", notes);
+
+                return params;
+            }
+        };
+
+        Volley.newRequestQueue(getApplicationContext()).add(postRequest);
+
+        //get confirmation of save
+
+        //return user to character screen
+        finish();
+
+    }
 }
