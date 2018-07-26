ea4666499f75edbdd861fa95906d1b701e2c08d1
johnb <johnabalbuza@gmail.com>
2018-07-26 10:36:37 -0400

    Fixed display newly created campaign

--- TavernTable/app/src/main/java/xyz/cop4331_7/taverntable/DMSelectionofCampaign.java
+++ TavernTable/app/src/main/java/xyz/cop4331_7/taverntable/DMSelectionofCampaign.java
@@ -46,17 +46,16 @@ public class DMSelectionofCampaign extends AppCompatActivity {
                         try {
                             JSONObject jsonResponseObject = new JSONObject(response);
                             //go get character names
-                            JSONArray characterNames = jsonResponseObject.getJSONArray("characterNames");
+
                             JSONArray campaignNames = jsonResponseObject.getJSONArray("campaignNames");
                             JSONArray campaignIDs = jsonResponseObject.getJSONArray("campaignIDs");
                             JSONArray dm = jsonResponseObject.getJSONArray("DMNames");
                             JSONArray partySize = jsonResponseObject.getJSONArray("partySizes");
                             JSONArray partyKey = jsonResponseObject.getJSONArray("partyKeys");
 
-                            for(int i=0; i<characterNames.length(); i++)
+                            for(int i=0; i<campaignNames.length(); i++)
                             {
-                                String charName = characterNames.getString(i);
-                                updateScroll("Character Name: " + charName);
+
                                 String dmS = dm.getString(i);
                                 updateScroll("DM: " + dmS);
                                 String campName = campaignNames.getString(i);
